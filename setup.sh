234b0f3d223e7fcf21f9f885bd54264aba258917
Mohit Yadav <meet2mky@gmail.com>
2025-03-30 10:52:22 +0000

    fix dotfiles

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-bash Installations/go/uninstall.sh
-bash Installations/go/install.sh
-
 bash Installations/zsh/install.sh
 
 bash Installations/oh-my-zsh/install.sh
 bash Installations/oh-my-zsh/customize.sh
+
+bash Installations/go/install.sh
+bash Installations/fuse/install.sh
