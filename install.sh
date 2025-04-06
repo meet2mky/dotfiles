f0d86c470498f1a674e05398e6ca258f6c49aea0
Mohit Yadav <meet2mky@gmail.com>
2025-04-06 14:25:57 +0000

    fix go version from install

--- install.sh
+++ install.sh
@@ -22,11 +22,24 @@ check_command() {
 }
 
 execute_script() {
+    if [ "$#" -eq 0 ]; then
+        log_error "Requires at least one argument." > &2
+        return 1
+    fi
+    
+    local SCRIPT_PATH="$1"
+    shift # Remove the first argument from the list
+
     log_info ""
     log_info ""
     log_info "---------------------------------------------------------------------"
     log_info "---------------------------------------------------------------------"
-    bash $1
+
+    if [ "$#" -gt 0 ]; then
+        bash $1 $@
+    else
+        bash $1
+    fi
     log_info "---------------------------------------------------------------------"
     log_info "---------------------------------------------------------------------"
     log_info ""
@@ -34,12 +47,24 @@ execute_script() {
 }
 
 execute_script "$HOME/dotfiles/Installations/zsh/install.sh"
-execute_script "$HOME/dotfiles/Installations/go/install.sh"
+execute_script "$HOME/dotfiles/Installations/go/install.sh" "1.24.0"
 execute_script "$HOME/dotfiles/Installations/fuse/install.sh"
 execute_script "$HOME/dotfiles/Installations/oh-my-zsh/install.sh"
 
 go run "vscode/main.go"
 
 execute_script "$HOME/dotfiles/monitor/install.sh"
+
+DIR_TO_INSTALL="/usr/local/bin"
+INSTALL_COMMAND_NAME="dotfiles_install"
+log_info ""
+log_info ""
+log_info "---------------------------------------------------------------------"
+log_info "Adding install script to $DIR_TO_INSTALL..."
+ln -s "$HOME/dotfiles/install.sh" "$DIR_TO_INSTALL/$INSTALL_COMMAND_NAME"
+check_command "$INSTALL_COMMAND_NAME"
+log_info "---------------------------------------------------------------------"
+log_info ""
+log_info ""
 
 exec zsh
